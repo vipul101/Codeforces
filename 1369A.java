5f87f7b49f89cc880885625dc41552f27186c45d
vipul101 <56825694+vipul101@users.noreply.github.com>
2021-11-23 19:56:49 +0530

    FashionabLee

---
+++ 1369A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class LeeFashion{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            if(n%4==0) System.out.println("YES");
+            else  System.out.println("NO");
+        }
+    }
+}
